edb226e3ad62b7871defd97322e5d8f3ba8fd2e9
Aleksandr <65610825+A-Apkhanov@users.noreply.github.com>
2022-02-20 17:08:16 +0300

    fix import description (#37)

--- src/components/organism/Description/Description.tsx
+++ src/components/organism/Description/Description.tsx
@@ -1,9 +1,9 @@
-import {FC} from 'react';
+import { FC } from 'react';
 
 import { Card } from 'antd';
 
-import styles from './Description.module.css'
-import './Description.css'
+import styles from './Description.module.css';
+import './Description.css';
 
 export const Description: FC = () => {
 	return (
@@ -11,28 +11,32 @@ export const Description: FC = () => {
 			<h3 className={`page__title ${styles.title}`}>Изучайте английский вместе с FreeTalk</h3>
 			<div className={styles.cardsWrapper}>
 				<Card
-					cover={<img className='card-cover' alt="translate" src="https://englex.ru/app/uploads/books-in-english-1.png" />}
+					cover={<img className='card-cover' alt='translate'
+											src='https://englex.ru/app/uploads/books-in-english-1.png' />}
 					className={`card-description ${styles.card}`}
 				>
 					<h3 className={styles.cardTitle}>Расширенный перевод</h3>
 					<p className={styles.cardDescription}>Подберем похожие слова дополнительно к основному переводу.</p>
 				</Card>
 				<Card
-					cover={<img className='card-cover' alt="dictionary" src="https://englex.ru/app/uploads/how-to-choose-english-dictionary.png" />}
+					cover={<img className='card-cover' alt='dictionary'
+											src='https://englex.ru/app/uploads/how-to-choose-english-dictionary.png' />}
 					className={`card-description ${styles.card}`}
 				>
 					<h3 className={styles.cardTitle}>Онлайн словарь</h3>
 					<p className={styles.cardDescription}>Персональный словарь всегда с вами и доступен с любого устройства.</p>
 				</Card>
 				<Card
-					cover={<img className='card-cover' alt="training" src="https://englex.ru/app/uploads/english-for-trainers-and-athletes.png" />}
+					cover={<img className='card-cover' alt='training'
+											src='https://englex.ru/app/uploads/english-for-trainers-and-athletes.png' />}
 					className={`card-description ${styles.card}`}
 				>
 					<h3 className={styles.cardTitle}>Тренировки</h3>
 					<p className={styles.cardDescription}>Поможем вам быстро выучить новые слова и повторить старые.</p>
 				</Card>
 				<Card
-					cover={<img className='card-cover' alt="collections" src="https://englex.ru/app/uploads/surround-yourself-with-english.png" />}
+					cover={<img className='card-cover' alt='collections'
+											src='https://englex.ru/app/uploads/surround-yourself-with-english.png' />}
 					className={`card-description ${styles.card}`}
 				>
 					<h3 className={styles.cardTitle}>Коллекции</h3>
